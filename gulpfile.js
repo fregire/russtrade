8d69bd15f5005d0eea31f541e1a529f6d3f0e340
fregire <fregire@mail.ru>
2018-05-26 22:15:45 +0500

    Таск для минификации css

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var less = require('gulp-less');
 var concat = require('gulp-concat');
 var plumber = require("gulp-plumber");
 var browser = require("browser-sync").create();
+var minifyCss = require("gulp-csso");
+var rename = require("gulp-rename");
 
 
 gulp.task('less', function(){
@@ -18,6 +20,17 @@ gulp.task('less', function(){
 		.pipe(browser.stream());
 
 });
+
+gulp.task("mincss", function(){
+	return gulp.src(["src/css/**/*.css", "!src/css/fonts.css"])
+		.pipe(minifyCss())
+		.pipe(rename({suffix: ".min"}))
+		.pipe(gulp.dest("src/css/min"));
+});
+
+gulp.task("concatjs", function(){
+	
+})
 
 gulp.task("serve", ['less'] , function() {
 	browser.init({
